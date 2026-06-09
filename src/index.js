c9e2564071b91c44a08f5768912b55223167c356
Johnny <espgoodlife22@gmail.com>
2026-06-09 21:25:55 +0300

    done

--- src/index.js
+++ src/index.js
@@ -2,17 +2,24 @@ import chalk from 'chalk';
 import { DB_PATH } from './constants.js';
 import fs from 'fs/promises';
 
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
 const readTasks = async () => {
-  return await fs.readFile(DB_PATH, 'utf-8').then(data => JSON.parse(data));
+  try {
+    return await fs.readFile(DB_PATH, 'utf-8').then(data => JSON.parse(data));
+  } catch (error) {
+    return [];
+  }
 };
 
 const writeTasks = async tasks => {
   await fs.writeFile(DB_PATH, JSON.stringify(tasks, null, 2));
 };
 
-console.log(chalk.hex('#d6d963')('all users ==>'));
+// console.log(chalk.hex('#d6d963')('all users ==>'));
 
-console.log(await readTasks());
+// console.log(await readTasks());
 
 const getById = async id => {
   const tasks = await readTasks();
@@ -24,20 +31,18 @@ const getById = async id => {
   return task;
 };
 
-console.log(chalk.hex('#d6d963')('get by Id 4 ==>'));
-console.log(await getById(4));
-
 const addNewTask = async newTask => {
   const tasks = await readTasks();
-  tasks.push(newTask);
+  const id =
+    (tasks.length === 0
+      ? 1
+      : tasks.reduce((acc, task) => {
+          return task.id > acc ? task.id : acc;
+        }, 0)) + 1;
+
+  tasks.push({ title: newTask, id, completed: false });
   await writeTasks(tasks);
 };
-
-await addNewTask({ id: 200, title: 'test', complited: true });
-
-console.log(chalk.hex('#d6d963')('add new item with id 200 ==>'));
-
-console.log(await readTasks());
 
 const deleteTask = async id => {
   const tasks = await readTasks();
@@ -45,19 +50,64 @@ const deleteTask = async id => {
   await writeTasks(filteredTasks);
 };
 
-await deleteTask(200);
-console.log(chalk.hex('#d6d963')('tasks after deleting task with the id 200'));
-console.log(await readTasks());
-
 const updateTask = async (id, newTask) => {
   const task = await getById(id);
   if (!task) return;
-  await deleteTask(id);
-  const tasks = await readTasks();
 
-  const updatedTask = { ...task, ...newTask };
-  tasks.push(updatedTask);
-  await writeTasks(tasks);
+  const tasks = await readTasks();
+  const updatedTasks = tasks.map(task =>
+    task.id === id ? { ...task, ...newTask } : task,
+  );
+
+  await writeTasks(updatedTasks);
 };
 
-await updateTask(15, { completed: true });
+yargs()
+  .command(
+    'list',
+    'get all tasks',
+    () => {},
+    async argv => {
+      const tasks = await readTasks();
+
+      console.log(tasks);
+    },
+  )
+  .command(
+    'get',
+    'get by id',
+    () => {},
+    async argv => {
+      const task = await getById(Number(argv.id));
+      console.log(task);
+    },
+  )
+  .command(
+    'add',
+    'add a task',
+    () => {},
+    async argv => {
+      await addNewTask(String(argv.title));
+      console.log(argv.title);
+    },
+  )
+  .command(
+    'update',
+    'update a task',
+    () => {},
+    async argv => {
+      const newTask = {};
+      if (argv.title !== undefined) newTask.title = argv.title;
+      if (argv.completed !== undefined) newTask.completed = argv.completed;
+      await updateTask(Number(argv.id), newTask);
+      console.log(newTask);
+    },
+  )
+  .command(
+    'remove',
+    'delete the task',
+    () => {},
+    argv => deleteTask(Number(argv.id)),
+  )
+  .help()
+  .parse(hideBin(process.argv));
